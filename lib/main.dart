6ba8040328c1b9234a8c855c59061bcaaf781081
Rafael WMS <rafael.wms@msn.com>
2020-02-10 12:06:55 -0300

    My solution

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,61 @@ class QuizPage extends StatefulWidget {
   _QuizPageState createState() => _QuizPageState();
 }
 
+class Question{
+  int index;
+  String question;
+  bool answer;
+
+  Question(int index, String question, bool answer){
+    this.index = index;
+    this.question = question;
+    this.answer = answer;
+  }
+}
+
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> answersIcons = [];
+
+  List<Question> questions =[
+    Question(0, 'You can lead a cow down stairs but not up stairs.', false),
+    Question(1, 'Approximately one quarter of human bones are in the feet.', true),
+    Question(2, 'A slug\'s blood is green.', true)
+  ];
+
+  int questionIndex = 0;
+
+  void nextQuestion() {
+    if((questionIndex + 1) < questions.length){
+      questionIndex++;
+    }else{
+      answersIcons = [];
+      questionIndex = 0;
+    }
+  }
+
+  void answerQuestion(bool answer){
+  setState(() {
+    if(answer == questions[questionIndex].answer){
+      answersIcons.add(correctAnswer());
+    }else{
+      answersIcons.add(wrongAnswer());
+    }
+    nextQuestion();
+  });
+
+  }
+
+  Icon correctAnswer(){
+    return Icon(Icons.check,
+    color: Colors.green);
+  }
+
+  Icon wrongAnswer(){
+    return Icon(Icons.close,
+        color: Colors.red);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +91,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionIndex].question,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +115,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                answerQuestion(true);
               },
             ),
           ),
@@ -79,12 +133,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                answerQuestion(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(children: answersIcons
+        ),
       ],
     );
   }
